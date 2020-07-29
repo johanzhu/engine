473ccbbd007d88ac5acf56d97ef6724080b0fc6b
胡松 <kevin.hs@antfin.com>
2020-07-29 16:37:08 +0800

    chore: add log

--- packages/component-miniprogram/src/index.js
+++ packages/component-miniprogram/src/index.js
@@ -23,12 +23,14 @@ Component({
       dispatchTouchEnd(e);
     },
     onCanvasReady() {
+      console.log("on canvas ready");
       const ctx2D = my.createCanvasContext("canvas2D");
       registerCanvas2D(ctx2D, "canvas2D");
       my._createCanvas({
         id: "canvas",
         success: canvas => {
           if (canvas) {
+            console.log("canvas onloaded");
             registerCanvas(canvas, "canvas");
             this.props.onLoad(o3, canvas);
           } else {
